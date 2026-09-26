6a25c51ea20ea3d698eba97e41a0f4008d7dda40
StudioBonventre <studiobonventre@gmail.com>
2026-09-26 07:34:59 +0200

    feat: wire Shortcuts description builder

--- src/automation/materialization.ts
+++ src/automation/materialization.ts
@@ -1,6 +1,7 @@
 import { capabilityV2 } from "./capabilityCatalogV2";
 import type { ShortcutDefinition, ShortcutStep } from "./shortcutCompiler";
 import { validateShortcutDefinition } from "./shortcutValidation";
+import { buildAppleIntelligenceAutomationDescription } from "./appleShortcutsHandoff";
 
 export const STORED_AUTOMATION_SCHEMA_VERSION = 2 as const;
 
