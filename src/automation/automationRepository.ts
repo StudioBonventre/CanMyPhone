5c878166814bacf6e6b9ef17369db2c131942595
StudioBonventre <studiobonventre@gmail.com>
2026-09-26 10:39:17 +0200

    feat: keep native location monitors synced with stored automations

--- src/automation/automationRepository.ts
+++ src/automation/automationRepository.ts
@@ -3,8 +3,14 @@ import { CanMyPhoneNative } from "../../modules/canmyphone-native";
 import { AutomationPersistence } from "./automationPersistence";
 
 export const automationRepository = new AutomationPersistence(AsyncStorage, {
-  async save(item){await CanMyPhoneNative?.syncAutomationDefinition(JSON.stringify(item));},
-  async remove(id){await CanMyPhoneNative?.deleteAutomationDefinition(id);}
+  async save(item){
+    await CanMyPhoneNative?.syncAutomationDefinition(JSON.stringify(item));
+    await CanMyPhoneNative?.syncLocationAutomations?.().catch(()=>undefined);
+  },
+  async remove(id){
+    await CanMyPhoneNative?.deleteAutomationDefinition(id);
+    await CanMyPhoneNative?.syncLocationAutomations?.().catch(()=>undefined);
+  }
 });
 
 export async function syncNativeRunnerResults() {
